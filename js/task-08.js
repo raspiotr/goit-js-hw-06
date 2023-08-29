6909e192a9305210ec2e9006a0231def915f0ad6
raspiotr <raspiotr@gmail.com>
2023-08-29 22:41:52 +0200

    task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector(".login-form");
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const email = form.elements.email;
+  const password = form.elements.password;
+
+  if (email.value === "" || password.value === "") {
+    return alert("You should fill in all the fields.");
+  }
+
+  const outputData = {
+    [email.name]: email.value,
+    [password.name]: password.value,
+  };
+  console.log(outputData);
+  form.reset();
+}
+
+loginForm.addEventListener("submit", handleSubmit);

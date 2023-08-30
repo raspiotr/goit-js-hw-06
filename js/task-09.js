319acab8746f9d5ec54ec947194b5952f085c07c
raspiotr <raspiotr@gmail.com>
2023-08-30 13:16:28 +0200

    task 09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const changeColorBtn = document.querySelector(".change-color");
+const colorText = document.querySelector(".color");
+
+const handleClick = () => {
+  const newColor = getRandomHexColor();
+  //console.log(newColor);
+  colorText.textContent = newColor;
+  body.style.backgroundColor = newColor;
+};
+
+changeColorBtn.addEventListener("click", handleClick);

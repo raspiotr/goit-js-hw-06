2a1a8b21aa95e11203801cf1130dcbb1521922a0
raspiotr <raspiotr@gmail.com>
2023-08-31 18:31:51 +0200

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -4,11 +4,27 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-// const input = document.querySelector("input");
-// const createBtn = document.querySelector("button[data-create]");
-// const destroyBtn = document.querySelector("button[data-destroy]");
-// const boxes = document.querySelector("#boxes");
+const input = document.querySelector("input");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const boxes = document.querySelector("#boxes");
 
-// input.addEventListener("change", (event) => {
-//   console.log(event.currentTarget.value);
-// });
+function createBoxes() {
+  const inputValue = input.valueAsNumber;
+
+  for (let i = 0; i < inputValue; i++) {
+    let divItem = document.createElement("div");
+    divItem.style.width = `${30 + i * 10}px`;
+    divItem.style.height = `${30 + i * 10}px`;
+    divItem.style.backgroundColor = getRandomHexColor();
+    divItem.style.marginTop = "10px";
+    boxes.append(divItem);
+  }
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
